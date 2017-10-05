772fbbf1c7d88a68f03cf0401acd4ae058a85e35
fndh <xavi_cat36@hotmail.com>
2017-10-05 12:44:44 +0200

    Minor changes

--- 02-Bubble/Aimer.cpp
+++ 02-Bubble/Aimer.cpp
@@ -9,7 +9,7 @@
 #define SHOOT_COOLDOWN 50.f						//Time between ball stop and next shot
 #define MAX_ANGLE 75.f/(360*(2*float(M_PI)))	//Angle will be 75 degrees -> radians
 
-void Aimer::init(const glm::ivec2 & tileMapPos, ShaderProgram & shaderProgram)
+void Aimer::init(const glm::ivec2 & tileMapPos, ShaderProgram & shaderProgram) //, BallManager & bmng)
 {
 	
 	_canShoot = true;
@@ -41,18 +41,20 @@ void Aimer::update(int deltaTime)
 		if (_canShoot) {
 			/*
 			_bmng->launchBall(_heldBall, _angle);
-			_heldBall = _bmng->getNextBall();
+			_heldBall = _bmng->getNextHeldBall();
 			_shootTime = SHOOT_COOLDOWN;
 			_canShoot = false;
 			*/
 		}
 	}
+	//_heldBall->update(deltaTime, _angle);
 	_shootTime = std::max(0, _shootTime - deltaTime);
-
+	_canShoot = (_shootTime == 0); //&& !_bmng->ballUpdatesLeft());
 
 }
 
 void Aimer::render()
 {
 	_sprite->render();
+	//_heldBall->render();
 }
